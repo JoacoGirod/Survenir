7d98ed3f61945b2a8bcdab9d8188c33fd0b9bd37
JoacoGirod <joaquingirod@gmail.com>
2020-07-19 21:56:09 -0300

    Inicio

---
+++ survenir/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+import './App.css';
+import Header from './components/Header.js'
+function App() {
+  return (
+    <Header creador = "Joaquin Girod" 
+    />
+  );
+}
+
+export default App;
---
+++ survenir/src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+function header (props){
+    return (
+    <h1 className='DIV000'>{props.creador}</h1>
+    
+    )
+}
+export default header
